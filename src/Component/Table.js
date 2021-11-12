f37eeb08be56019386351b75b3244a479601fac2
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-11-12 22:44:07 +0200

    get data from API

--- src/Component/Table.js
+++ src/Component/Table.js
@@ -1,10 +1,26 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 // import components
 
 const Table = () => {
-    return(
-        <div></div>
+
+    const [responseData , setResponseData] = useState([]);
+    const getDta = () => {
+        axios.get('https://api.most.technology/course/')
+            .then((response) => {
+                const myData = response.data;
+                setResponseData(myData);
+                // pass data response to state
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+    useEffect(() => getDta(), [])
+
+    return (
+        <div>{responseData.size}</div>
     )
 };
 

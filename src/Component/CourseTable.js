c0c24ae67f883c2189f62c28e59990f540dd68ac
Maria Ghorbani <ghorbanimoghaddam.m@gmail.com>
2021-11-13 11:36:42 -0500

    sort functionality

--- src/Component/CourseTable.js
+++ src/Component/CourseTable.js
@@ -10,13 +10,14 @@ import classes from './Caret.module.css'
 
 const CourseTable = () => {
 
-    const [responseData, setResponseData] = useState([]);
-    const Courses = responseData.map((item) => item);
-    const [sortIcon, setSortIcon] = useState(false);
+    const [courses, setCourses] = useState([]);
+    const [sortASC, setSortASC] = useState(false);
 
     const getDta = () => {
         axios.get('https://api.most.technology/course/').then(response => response.data).then((data) => {
-            setResponseData(data.data);
+            setCourses(prevState => {
+                return sorter([...prevState, ...data.data], sortASC);
+            });
             // pass data response to state
         })
             .catch(function (error) {
@@ -25,43 +26,51 @@ const CourseTable = () => {
     }
     useEffect(() => getDta(), [])
 
-    const changeOrder = () => {
-        Courses.sort((a, b) => {
-            setSortIcon(!sortIcon);
-            console.log("a:", a.title, "b:", b.title)
+    const sorter = (items, asc=true) => {
+        return items.sort((a, b) => {
+            const sortK = asc ? 1 : -1
             if (a.title < b.title) {
-                return -1;
-            }
-            if (a.title > b.title) {
-                return 1;
+                return 1 * sortK;
+            } else if (a.title > b.title) {
+                return -1 * sortK;
             }
             return 0;
         });
+    }
+
+    const changeOrder = () => {
+        setCourses(prevState => {
+            const newSort = !sortASC;
+            setSortASC(newSort)
+            return sorter(courses, newSort)
+        });
     };
 
-    return (<Card>
-        <Table striped bordered hover>
-            <thead>
-            <tr>
-                <th onClick={changeOrder}>
-                    Title
-                    <span className={sortIcon ? classes.headerSortUp : classes.headerSortDown}></span>
-                </th>
-                <th>Start Date</th>
-                <th>End Date</th>
-                <th>Price</th>
-            </tr>
-            </thead>
-            <tbody>
-            {responseData.map((item) => (<tr key={item.id}>
-                <td>{item.title}</td>
-                <td>{item.start}</td>
-                <td>{item.end}</td>
-                <td>{item.price}</td>
-            </tr>))}
-            </tbody>
-        </Table>
-    </Card>)
+    return (
+        <Card>
+            <Table responsive striped bordered hover>
+                <thead>
+                <tr>
+                    <th onClick={changeOrder}>
+                        Title
+                        <span className={sortASC ? classes.headerSortUp : classes.headerSortDown}></span>
+                    </th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>
+                {courses.map((item) => (<tr key={item.id}>
+                    <td>{item.title}</td>
+                    <td>{item.start}</td>
+                    <td>{item.end}</td>
+                    <td>{item.price}</td>
+                </tr>))}
+                </tbody>
+            </Table>
+        </Card>
+    )
 };
 
 export default CourseTable;

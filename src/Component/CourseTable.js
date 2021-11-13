3a1b1da1e7a2fde15010a187cfea9d179e894c16
Maria Ghorbani <ghorbanimoghaddam.m@gmail.com>
2021-11-13 09:50:07 -0500

    add sort caret in title header

--- src/Component/CourseTable.js
+++ src/Component/CourseTable.js
@@ -1,43 +1,63 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import {Table} from 'react-bootstrap';
 
 // import components
 import Card from "./UI/Card";
 
-import { Table } from 'react-bootstrap';
+//import styles
+import classes from './Caret.module.css'
 
 const CourseTable = () => {
 
-    const [responseData , setResponseData] = useState([]);
+    const [responseData, setResponseData] = useState([]);
+    const Courses = responseData.map((item) => item);
+    const [sortIcon, setSortIcon] = useState(false);
+
     const getDta = () => {
         axios.get('https://api.most.technology/course/').then(response => response.data).then((data) => {
-                setResponseData(data.data);
-                // pass data response to state
-            })
+            setResponseData(data.data);
+            // pass data response to state
+        })
             .catch(function (error) {
                 console.log(error);
             });
     }
     useEffect(() => getDta(), [])
 
+    const changeOrder = () => {
+        Courses.sort((a, b) => {
+            setSortIcon(!sortIcon);
+            console.log("a:", a.title, "b:", b.title)
+            if (a.title < b.title) {
+                return -1;
+            }
+            if (a.title > b.title) {
+                return 1;
+            }
+            return 0;
+        });
+    };
+
     return (<Card>
         <Table striped bordered hover>
             <thead>
             <tr>
-                <th>Title</th>
+                <th onClick={changeOrder}>
+                    Title
+                    <span className={sortIcon ? classes.headerSortUp : classes.headerSortDown}></span>
+                </th>
                 <th>Start Date</th>
                 <th>End Date</th>
                 <th>Price</th>
             </tr>
             </thead>
             <tbody>
-            {console.log(responseData)}
             {responseData.map((item) => (<tr key={item.id}>
                 <td>{item.title}</td>
                 <td>{item.start}</td>
                 <td>{item.end}</td>
                 <td>{item.price}</td>
-                <td/>
             </tr>))}
             </tbody>
         </Table>
